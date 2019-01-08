e9f9be91f1ab288a58791cd42d11b693422abe13
江珮妤 <Luxgen077@gmail.com>
2019-01-08 18:12:11 +0800

    預約訂位&預約成功&查詢預約頁面

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,8 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('title', '線上訂位')
 
 @section('content')
 
     <!-- Bootstrap 樣板... -->
+    <header class="intro-header" style="background-image: url('img/reservation.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="page-heading">
+                        <h1>線上訂位</h1>
+                        <hr class="small">
+                        <span class="subheading">Online Reservation</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <div class="panel-body">
         <!-- 顯示驗證錯誤 -->
@@ -14,62 +29,65 @@
 
         <!-- 任務名稱 -->
             <div class="form-group">
-                <label for="task-name" class="col-sm-3 control-label">任務</label>
+                <label for="task-name" class="col-sm-3 control-label">姓名</label>
 
                 <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                    <input type="text" name="name" id="task-name" value="{{Auth::user()->name}}" class="form-control">
                 </div>
             </div>
 
+            <div class="form-group">
+                <label for="task-phone" class="col-sm-3 control-label">電話</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="phone" id="task-phone" value="{{Auth::user()->phone}}" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="task-people" class="col-sm-3 control-label">人數</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="people" id="task-people" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="task-store" class="col-sm-3 control-label">店家</label>
+                <div class="col-sm-6">
+                    <select name="store" class="form-control">
+                        <option value="文心店">文心店</option>
+                        <option value="崇德店">崇德店</option>
+                        <option value="公益店">公益店</option>
+                        <option value="台南店">台南店</option>
+                        <option value="博愛店">博愛店</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="task-date" class="col-sm-3 control-label">日期</label>
+
+                <div class="col-sm-6">
+                    <input type="date" name="date" value="<?php echo date("Y-m-d",strtotime('+1day'));?>" min="<?php echo date("Y-m-d",strtotime('+1day'));?>" max="<?php echo date("Y-m-d",strtotime('+6day'));?>" id="task-date" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="task-time" class="col-sm-3 control-label">時間</label>
+
+                <div class="col-sm-6">
+                    <input type="time" name="time" id="task-time" class="form-control">
+                </div>
+            </div>
             <!-- 增加任務按鈕-->
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> 增加任務
+                    <button type="submit" class="btn btn-success">
+                        <i class="fa fa-plus"></i> 確認訂位
                     </button>
                 </div>
             </div>
         </form>
     </div>
-
-    <!-- 目前任務 -->
-    @if (count($tasks) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                目前任務
-            </div>
-
-            <div class="panel-body">
-                <table class="table table-striped task-table">
-
-                    <!-- 表頭 -->
-                    <thead>
-                    <th>Task</th>
-                    <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- 表身 -->
-                    <tbody>
-                    @foreach ($tasks as $task)
-                        <tr>
-                            <!-- 任務名稱 -->
-                            <td class="table-text">
-                                <div>{{ $task->name }}</div>
-                            </td>
-                            <!-- 刪除按鈕 -->
-                            <td>
-                                <form action="/task/{{ $task->id }}" method="POST">
-                                    {{ csrf_field() }}
-                                    {{ method_field('DELETE') }}
-
-                                    <button>刪除任務</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    @endif
 @endsection
