62194e566ff523e6aed48b92f9c88167de8eb800
江珮妤 <Luxgen077@gmail.com>
2019-01-08 12:11:37 +0800

    首頁

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>輕井澤</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -12,8 +12,9 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-image: url('img/welcome.jpg');
+                background-size:100%;
+                color:#fff;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -45,13 +46,13 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 100px;
             }
 
             .links > a {
-                color: #636b6f;
+                color: #fff;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 25px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -68,12 +69,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">會員專區</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">登入</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">註冊</a>
                         @endif
                     @endauth
                 </div>
@@ -81,18 +82,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    輕井澤
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                   <a href="{{route('store.index')}}"> 店家資訊</a>
+                    <a href="{{route('meal.index')}}">餐點介紹</a>
+                    <a href="{{route('task.index')}}">線上訂位</a>
+                    <a href="{{route('wait.index')}}">查詢現場候位</a>
+                    <a href="{{route('post.show')}}">最新消息</a>
+
                 </div>
             </div>
-        </div>
     </body>
 </html>
