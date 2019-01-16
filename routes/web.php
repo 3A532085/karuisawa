764f1165b3557272696a500b21e458173e0a469c
江珮妤 <Luxgen077@gmail.com>
2019-01-17 01:08:48 +0800

    路徑

--- routes/web.php
+++ routes/web.php
@@ -42,17 +42,42 @@ Route::get('reservationok',  ['as' => 'reservation.show',    'uses' => 'Reservat
 Route::get('searchreservation',  ['as' => 'searchreservation.show',    'uses' => 'SearchReservationController@show']);
 Route::get('welcome',  ['as' => 'welcome.index',    'uses' => 'HomeController@index']);
 Route::get('member',  ['as' => 'member.show',    'uses' => 'MemberController@show']);
-Route::get('members/{id}',  ['as' => 'member.index',    'uses' => 'MemberController@index']);
+Route::get('/members/{id}',  ['as' => 'member.index',    'uses' => 'MemberController@index']);
 Route::get('editmember/{id}',  ['as' => 'editmember.edit',    'uses' => 'MemberController@edit']);
 Route::patch('/member/update/{id}', 'MemberController@update');
+Route::get('updatemember',  ['as' => 'updatemember.new',    'uses' => 'MemberController@new']);
 Route::get('/staff', 'StaffController@index');
 Route::get('/allreservation', 'AllReservationController@index');
+Route::get('allreservation',  ['as' => 'allreservation.index',    'uses' => 'AllReservationController@index']);
 Route::delete('/allreservation/{id}', 'AllReservationController@destroy');
 Route::get('wait',  ['as' => 'wait.index',    'uses' => 'WaitController@index']);
+//Route::get('wait',  ['as' => 'wait.index2',    'uses' => 'WaitController@index2']);
 Route::post('/wenxins/{id}', 'WaitController@store');
 Route::get('/insertwait/{id}', 'WaitController@insert');
 Route::get('insertwait',  ['as' => 'wait.insert',    'uses' => 'WaitController@insert']);
 Route::get('finishwait',  ['as' => 'wait.finish',    'uses' => 'WaitController@finish']);
 Route::get('/success', 'WaitController@finish');
 Route::patch('/task/ok/{id}', 'TaskController@ok');
-Route::get('/allwait', 'AllwaitController@index');
+Route::get('/searchwait', 'SearchWaitController@index');
+Route::get('searchwait',  ['as' => 'searchwait.index',    'uses' => 'SearchWaitController@index']);
+Route::get('/allwait/{id}', 'AllwaitController@index');
+Route::get('allwait',  ['as' => 'allwait.index',    'uses' => 'AllwaitController@index']);
+Route::patch('/wenxin/allwait/{id}', 'SearchWaitController@ok');
+Route::patch('/wenxin/allwait/{id}', 'SearchWaitController@ok');
+Route::delete('/wenxin/delete/{id}', 'SearchWaitController@destroy');
+//Route::patch('wait',  ['as' => 'wait.update',    'uses' => 'WaitController@update']);
+Route::patch('/wait/update', 'WaitController@update');
+Route::get('/admin', 'AdminController@index');
+Route::get('showcount',  ['as' => 'admin.index',    'uses' => 'AdmminController@index']);
+Route::get('storemember',  ['as' => 'member.new',    'uses' => 'MemberController@new']);
+Route::get('/member/update/{id}', 'MemberController@new');
+Route::patch('/wenxin/waittime/{id}', 'TimenewController@update');
+//Route::patch('/wenxin/waittime/{id}', 'WaitController@update');
+Route::post('/wenxins/store', 'WaitController@store2');
+Route::get('/minus', 'TimenewController@minus');
+Route::patch('/wenxin/num/{id}', 'TimenewController@num');
+Route::get('post',  ['as' => 'post.index',    'uses' => 'PostController@index']);
+Route::patch('/wenxin/post/{id}', 'TimenewController@post');
+
+
+
